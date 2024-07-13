9d8053595075d83cc64f610275c32bc998c4f55c
afiniel <afitrade@hotmail.com>
2024-07-13 16:10:21 -0700

    Update install.sh
    Tag v0.0.3

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@
 set -e
 
 # Set the default TAG if not provided
-TAG=${TAG:-v0.0.2}
+TAG=${TAG:-v0.0.3}
 
 # Create the yiimpoolversion.conf file
 echo "VERSION=${TAG}" | sudo tee /etc/yiimpoolversion.conf >/dev/null
