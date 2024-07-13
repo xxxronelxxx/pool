1a081680d70c6d32302394fee0e78e3f4413e468
afiniel <afitrade@hotmail.com>
2024-07-13 16:08:17 -0700

    Update functions.sh

--- install/functions.sh
+++ install/functions.sh
@@ -32,44 +32,6 @@ spinner() {
     printf "    \b\b\b\b"
 }
 
-
-# Install.sh functions.
-
-# Function to install git if it's not already installed
-install_git() {
-  if ! command -v git &>/dev/null; then
-    echo "Installing git..."
-    apt-get -q update
-    DEBIAN_FRONTEND=noninteractive apt-get -q install -y git
-    echo "Git installed."
-  fi
-}
-
-# Function to clone the Yiimpool installer repository
-clone_repo() {
-  if [ ! -d "$HOME/Yiimpoolv2" ]; then
-    echo "Downloading Yiimpool Installer ${TAG}..."
-    git clone -b "${TAG}" --depth 1 https://github.com/afiniel/Yiimpoolv2 "$HOME/Yiimpoolv2"
-    echo "Repository cloned."
-  fi
-}
-
-# Function to update the Yiimpool installer repository
-update_repo() {
-  cd "$HOME/Yiimpoolv2"
-
-  sudo chown -R "$USER" "$HOME/Yiimpoolv2/.git/"
-  if [ "${TAG}" != "$(git describe --tags)" ]; then
-    echo "Updating Yiimpool Installer to ${TAG}..."
-    git fetch --depth 1 --force --prune origin tag "${TAG}"
-    if ! git checkout -q "${TAG}"; then
-      echo "Update failed. Did you modify something in $(pwd)?"
-      exit 1
-    fi
-    echo "Repository updated."
-  fi
-}
-
 # MESSAGE BOX FUNCTIONS.
 
 # Function to start the Yiimpool installation script
