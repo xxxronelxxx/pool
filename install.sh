52f5209d5965de673493ca76aa4a304397c62a1c
afiniel <afitrade@hotmail.com>
2024-07-13 16:09:38 -0700

    Update install.sh
    Removed the install functions in the functions.sh and enter it back to install.sh file

--- install.sh
+++ install.sh
@@ -16,14 +16,49 @@
 
 set -e
 
-# Include the functions file
-source "$HOME/yiimp_install_script/functions.sh"
-
 # Set the default TAG if not provided
 TAG=${TAG:-v0.0.2}
 
-# Main script execution
+# Create the yiimpoolversion.conf file
 echo "VERSION=${TAG}" | sudo tee /etc/yiimpoolversion.conf >/dev/null
+
+# Function to install git if it's not already installed
+install_git() {
+  if ! command -v git &>/dev/null; then
+    echo "Installing git..."
+    apt-get -q update
+    DEBIAN_FRONTEND=noninteractive apt-get -q install -y git
+    echo "Git installed."
+  fi
+}
+
+# Function to clone the Yiimpool installer repository
+clone_repo() {
+  if [ ! -d "$HOME/Yiimpoolv2" ]; then
+    echo "Downloading Yiimpool Installer ${TAG}..."
+    git clone -b "${TAG}" --depth 1 https://github.com/afiniel/Yiimpoolv2 "$HOME/Yiimpoolv2"
+    echo "Repository cloned."
+  fi
+}
+
+# Function to update the Yiimpool installer repository
+update_repo() {
+  cd "$HOME/Yiimpoolv2"
+
+  sudo chown -R "$USER" "$HOME/Yiimpoolv2/.git/"
+  if [ "${TAG}" != "$(git describe --tags)" ]; then
+    echo "Updating Yiimpool Installer to ${TAG}..."
+    git fetch --depth 1 --force --prune origin tag "${TAG}"
+    if ! git checkout -q "${TAG}"; then
+      echo "Update failed. Did you modify something in $(pwd)?"
+      exit 1
+    fi
+    echo "Repository updated."
+  fi
+}
+
+# Include the functions file
+#source "$HOME/yiimp_install_script/functions.sh"
 
 install_git
 clone_repo
