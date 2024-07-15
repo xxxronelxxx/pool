71255de198d892d4d99286aa1d4697cbcf6c9cd3
afiniel <afitrade@hotmail.com>
2024-07-15 07:20:30 -0700

    Screens

--- yiimp_single/motd.sh
+++ yiimp_single/motd.sh
@@ -4,30 +4,31 @@
 # Created by Afiniel for Yiimpool use...
 #####################################################
 
-source /etc/functions.sh # load our functions
+# Source necessary configuration files
+source /etc/functions.sh
 source /etc/yiimpool.conf
 
-apt_install lsb-release figlet update-motd \
-    landscape-common update-notifier-common
-cd $HOME/Yiimpoolv2/yiimp_single/ubuntu/etc/update-motd.d
-sudo rm -r /etc/update-motd.d/
-sudo mkdir /etc/update-motd.d/
-sudo touch /etc/update-motd.d/00-header
-sudo touch /etc/update-motd.d/10-sysinfo
-sudo touch /etc/update-motd.d/90-footer
-sudo chmod +x /etc/update-motd.d/*
-sudo cp -r 00-header 10-sysinfo 90-footer /etc/update-motd.d/
+# Install required packages
+apt_install lsb-release figlet update-motd landscape-common update-notifier-common
 
-# copy additional files
-cd $HOME/Yiimpoolv2/yiimp_single/ubuntu/
-sudo cp -r screens /usr/bin/
+# Set up/update MOTD (Message of the Day)
+cd "$HOME/Yiimpoolv2/yiimp_single/ubuntu/etc/update-motd.d"
+sudo rm -rf /etc/update-motd.d/
+sudo mkdir /etc/update-motd.d/
+sudo cp -r {00-header,10-sysinfo,90-footer} /etc/update-motd.d/
+sudo chmod +x /etc/update-motd.d/*
+
+# Copy additional scripts to system binaries
+sudo cp -r "$HOME/Yiimpoolv2/yiimp_single/ubuntu/screens" /usr/bin/
 sudo chmod +x /usr/bin/screens
-sudo cp -r stratum /usr/bin
+sudo cp -r "$HOME/Yiimpoolv2/yiimp_single/ubuntu/stratum" /usr/bin/
 sudo chmod +x /usr/bin/stratum
+
+# Create and configure custom motd command
 echo '
 clear
 run-parts /etc/update-motd.d/ | sudo tee /etc/motd
 ' | sudo -E tee /usr/bin/motd >/dev/null 2>&1
-
 sudo chmod +x /usr/bin/motd
-cd $HOME/Yiimpoolv2/yiimp_single
+
+cd "$HOME/Yiimpoolv2/yiimp_single"
