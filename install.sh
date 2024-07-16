9f74b2a7ebc12e3ba76d6532d5f6896ac1918dc0
afiniel <afitrade@hotmail.com>
2024-07-16 03:59:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v0.2.1}
+TAG=${TAG:-v0.2.2}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
