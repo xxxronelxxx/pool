11f5a800e746e296ed7bb98727a1269d1dfdb6a6
afiniel <afitrade@hotmail.com>
2025-01-08 20:38:38 +0100

    Update Ver 2.5.5

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v0.2.4}
+TAG=${TAG:-v0.2.5}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
