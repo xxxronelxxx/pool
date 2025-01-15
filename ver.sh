e610fec155e8f4202afce66417c474519768c904
afiniel <afitrade@hotmail.com>
2025-01-15 19:07:42 +0100

    Create ver.sh

---
+++ ver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#########################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by Afiniel for Yiimpool use...
+# This script is intended to be run like this:
+#
+# curl https://raw.githubusercontent.com/afiniel/yiimp_install_script/master/install.sh | bash
+#
+#########################################################
+
+if [ -z "${TAG}" ]; then
+	TAG=v2.3.1
+fi
+
+echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
