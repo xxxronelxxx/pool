5f121c70fb508f46badbbfbb750e4f0b47911e22
afiniel <afitrade@hotmail.com>
2025-01-16 11:01:05 +0100

    Set Ver to v2.3.2

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v2.3.1}
+TAG=${TAG:-v2.3.2}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
