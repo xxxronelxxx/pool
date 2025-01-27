fb913f5c915e88c504f444e799add23c9914467b
afiniel <afitrade@hotmail.com>
2025-01-27 23:55:11 +0100

    2.3.4

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v2.3.3}
+TAG=${TAG:-v2.3.4}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
--- ver.sh
+++ ver.sh
@@ -9,7 +9,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v2.3.3
+	TAG=v2.3.4
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
