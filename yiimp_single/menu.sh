5e7cfed183981e409dc7bff72fda46b74e3d026f
afiniel <afitrade@hotmail.com>
2025-02-02 20:48:18 +0100

    Fix issue #21 And correct DB import.

--- yiimp_single/menu.sh
+++ yiimp_single/menu.sh
@@ -12,7 +12,7 @@ source /etc/yiimpooldonate.conf
 source /etc/functions.sh
 
 RESULT=$(dialog --stdout --default-item 1 --title "YiimPool Yiimp Installer $VERSION" --menu "choose an option" -1 55 7 \
-    ' ' "- Do you want to install Yiimp with whireguard? -" \
+    ' ' "- Do you want to install Yiimp with WireGuard? -" \
     1 "Yes" \
     2 "No" \
     3 exit)
@@ -21,7 +21,8 @@ case "$RESULT" in
     1)
         clear;
         echo 'wireguard=true' | sudo -E tee "$HOME"/Yiimpoolv1/yiimp_single/.wireguard.install.cnf >/dev/null 2>&1;
-        echo 'DBInternalIP=10.0.0.2' | sudo -E tee "$STORAGE_ROOT"/yiimp/.wireguard_public.conf >/dev/null 2>&1;
+        echo 'DBInternalIP=10.0.0.2
+              server_type=db' | sudo -E tee "$STORAGE_ROOT"/yiimp/.wireguard.conf >/dev/null 2>&1;
         ;;
     2)
         clear;
