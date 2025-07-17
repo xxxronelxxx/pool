57c39201fb771b2719ca07ec2d51ae1f8fd8facf
afiniel <44875524+afiniel@users.noreply.github.com>
2025-07-17 15:41:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v2.5.1}
+TAG=${TAG:-v2.5.2}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
